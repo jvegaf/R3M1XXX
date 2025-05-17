aa081f4d51137afe4e41892a108b471d0a22aa36
Jose Vega <josevega234@gmail.com>
2025-05-17 06:21:03 +0200

    feat: support multiple encoders in I2C test
    Refactor the code to handle multiple encoders instead of a single one.
    - Replace single encoder instance with an array of encoders.
    - Add arrays to track encoder values and pressed states.
    - Update setup and loop functions to iterate over all encoders.
    
    This change enables testing of up to three encoders simultaneously.

--- Code/cpp/encoder_i2c_test/src/main.cpp
+++ Code/cpp/encoder_i2c_test/src/main.cpp
@@ -3,12 +3,12 @@
 
 // #define encoder_addr 0x38 // 0x32 or 0x36 or 0x38
 
-byte encoder_addr = 0x38; // I2C address of the encoder
+byte enc_addrs[] = {0x32, 0x36, 0x38};
+int16_t pValues[] = {0, 0, 0};
+boolean pPressed[] = {false, false, false};
 
-int16_t value, lastvalue;
-boolean pressed, lastpressed;
-
-Encoder encoder(encoder_addr, Wire);
+Encoder encoders[] = {Encoder(enc_addrs[0], Wire), Encoder(enc_addrs[1], Wire),
+                      Encoder(enc_addrs[2], Wire)};
 
 void setup() {
   Serial.begin(9600);
@@ -16,29 +16,44 @@ void setup() {
     ;
   Serial.println("I2C Encoder Test");
   Wire.begin();
-  encoder.begin();
+  for (int i = 0; i < 3; i++) {
+    encoders[i].begin();
+    encoders[i].setValue(0);
+    Serial.print("Encoder ");
+    Serial.print(i);
+    Serial.print(" address: ");
+    Serial.println(enc_addrs[i], HEX);
+  }
   Serial.println("Encoder set");
 }
 
 void loop() {
-  value = encoder.getValue();
-  if (value != lastvalue) {
-    Serial.println("Value changed");
-    Serial.println(value);
-    if (value > 30) {
-      value = 0;
-      encoder.setValue(value);
-    } else if (value < -30) {
-      value = 0;
-      encoder.setValue(value);
+  for (int i = 0; i < 3; i++) {
+    int16_t value = encoders[i].getValue();
+    boolean pressed = encoders[i].isPressed();
+    if (pValues[i] != value) {
+      pValues[i] = value;
+      Serial.print("Encoder ");
+      Serial.print(i);
+      Serial.println(" value changed");
+      if (pValues[i] > 30) {
+        pValues[i] = 0;
+        encoders[i].setValue(pValues[i]);
+      } else if (pValues[i] < -30) {
+        pValues[i] = 0;
+        encoders[i].setValue(pValues[i]);
+      }
+      Serial.print("Value: ");
+      Serial.println(pValues[i]);
     }
-    lastvalue = value;
+    if (pPressed[i] != pressed) {
+      if (pPressed[i]) {
+        Serial.print("Encoder ");
+        Serial.print(i);
+        Serial.println(" switch was pressed");
+      }
+      pPressed[i] = pressed;
+    }
   }
-  pressed = encoder.isPressed();
-  if (pressed != lastpressed) {
-    if (pressed)
-      Serial.println("Switch was pressed");
-    lastpressed = pressed;
-  }
-  dkqqelay(20);
+  delay(20);
 }
