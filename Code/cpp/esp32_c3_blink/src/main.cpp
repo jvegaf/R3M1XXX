fe0547b1cb2fbb7d337925e9337b8ad99ec736b7
Jose Vega <josevega234@gmail.com>
2025-08-20 23:47:27 +0200

    esp32 c3 test fixes

--- Code/cpp/esp32_c3_blink/src/main.cpp
+++ Code/cpp/esp32_c3_blink/src/main.cpp
@@ -1,21 +1,8 @@
 #include <Arduino.h>
-/*
-  Blink
-
-  Flashes a LED every second, repeatedly.
-
-  The ESP32-C3 SuperMini has an on-board LED you can control. 
-  It is attached to digital pin 8. 
-  LED_BUILTIN is set to the correct LED pin.
-
-  This example code is in the public domain.
-
-  Adapted from:
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
 
 // the LED is at pin  GPIO8
 #define LED_BUILTIN 8
+#define SLEEP_TIME 200
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -25,8 +12,9 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED on (LOW is the voltage level)
-  delay(50);                      // wait 50 ms
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED off by making the voltage HIGH
-  delay(950);                      // wait 950 ms
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED on (LOW is the voltage level)
+  delay(SLEEP_TIME);              // wait 50 ms
+  digitalWrite(LED_BUILTIN,
+               HIGH); // turn the LED off by making the voltage HIGH
+  delay(SLEEP_TIME);  // wait 950 ms
 }
