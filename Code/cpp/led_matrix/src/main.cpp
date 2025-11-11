bd47df29e4421a4d77dee95f1f3adb11be8257f9
Jose Vega <josevega234@gmail.com>
2025-11-11 02:55:01 +0100

    micropython

--- Code/cpp/led_matrix/src/main.cpp
+++ Code/cpp/led_matrix/src/main.cpp
@@ -1,88 +1,44 @@
-#include "Adafruit_LEDBackpack.h"
-#include <Adafruit_GFX.h>
 #include <Arduino.h>
-#include <Wire.h>
+#include <NeoPixelConnect.h>
 
-Adafruit_8x16matrix matrix = Adafruit_8x16matrix();
-int DELAY_T = 1000;
+// Create an instance of NeoPixelConnect and initialize it
+// to use GPIO pin 4 (D12) as the control pin, for a string
+// of 8 neopixels. Name the instance p
+NeoPixelConnect p(4, 8);
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("16x8 LED Matrix Test");
+// this array will hold a pixel number and the rgb values for the
+// randomly generated pixel values
+uint8_t random_pixel_setting[4];
 
-  matrix.begin(0x70); // pass in the address
-  matrix.setBrightness(1);
+// select a random pixel number in the string
+uint8_t get_pixel_number() { return ((uint8_t)random(0, 7)); }
+
+// select a random intensity
+uint8_t get_pixel_intensity() { return ((uint8_t)random(0, 255)); }
+
+void get_random_pixel_and_color() {
+  random_pixel_setting[0] = get_pixel_number();
+  random_pixel_setting[1] = get_pixel_intensity();
+  random_pixel_setting[2] = get_pixel_intensity();
+  random_pixel_setting[3] = get_pixel_intensity();
 }
 
-static const uint8_t PROGMEM smile_bmp[] = {B00111100, B01000010, B10100101,
-                                            B10000001, B10100101, B10011001,
-                                            B01000010, B00111100},
-                             neutral_bmp[] = {B00111100, B01000010, B10100101,
-                                              B10000001, B10111101, B10000001,
-                                              B01000010, B00111100},
-                             frown_bmp[] = {B00111100, B01000010, B10100101,
-                                            B10000001, B10011001, B10100101,
-                                            B01000010, B00111100};
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  Serial.println("In setup");
+}
 
 void loop() {
 
-  matrix.clear();
-  matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_ON);
-  matrix.writeDisplay();
-  delay(DELAY_T);
+  // get a pixel number
+  get_random_pixel_and_color();
 
-  matrix.clear();
-  matrix.drawBitmap(0, 8, neutral_bmp, 8, 8, LED_ON);
-  matrix.writeDisplay();
-  delay(DELAY_T);
-
-  matrix.clear();
-  matrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_ON);
-  matrix.writeDisplay();
-  delay(DELAY_T);
-
-  matrix.clear(); // clear display
-  matrix.drawPixel(0, 0, LED_ON);
-  matrix.writeDisplay(); // write the changes we just made to the display
-  delay(DELAY_T);
-
-  matrix.clear();
-  matrix.drawLine(0, 0, 7, 15, LED_ON);
-  matrix.writeDisplay(); // write the changes we just made to the display
-  delay(DELAY_T);
-
-  matrix.clear();
-  matrix.drawRect(0, 0, 8, 16, LED_ON);
-  matrix.fillRect(2, 2, 4, 12, LED_ON);
-  matrix.writeDisplay(); // write the changes we just made to the display
-  delay(DELAY_T);
-
-  matrix.clear();
-  matrix.drawCircle(3, 8, 3, LED_ON);
-  matrix.writeDisplay(); // write the changes we just made to the display
-  delay(DELAY_T);
-
-  matrix.setTextSize(2);
-  matrix.setTextWrap(false); // we dont want text to wrap so it scrolls nicely
-  matrix.setTextColor(LED_ON);
-  for (int8_t x = 0; x >= -64; x--) {
-    matrix.clear();
-    matrix.setCursor(x, 0);
-    matrix.print("Hello");
-    matrix.writeDisplay();
-    delay(DELAY_T);
-  }
-
-  matrix.setTextSize(1);
-  matrix.setTextWrap(false); // we dont want text to wrap so it scrolls nicely
-  matrix.setTextColor(LED_ON);
-  matrix.setRotation(1);
-  for (int8_t x = 7; x >= -36; x--) {
-    matrix.clear();
-    matrix.setCursor(x, 0);
-    matrix.print("World");
-    matrix.writeDisplay();
-    delay(DELAY_T);
-  }
-  matrix.setRotation(0);
+  // display the randomly assigned pixel and color
+  p.neoPixelSetValue(random_pixel_setting[0], random_pixel_setting[1],
+                     random_pixel_setting[2], random_pixel_setting[3], true);
+  delay(100);
+  // clear all pixels
+  p.neoPixelClear();
+  delay(100);
 }
